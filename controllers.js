7bb1ef643bb68657c7e477a18a524d16d89ab342
Jelena Marjanović <jelena.marjanovic.13@gmail.com>
2019-01-25 23:27:47 +0100

    Make reusable custom service to fetch data and inject it to our controller

--- controllers.js
+++ controllers.js
@@ -19,31 +19,15 @@ weatherApp.controller('homeController', [
 
 weatherApp.controller('forecastController', [
   '$scope',
-  '$resource',
   '$routeParams',
   'cityService',
-  function($scope, $resource, $routeParams, cityService) {
+  'weatherService',
+  function($scope, $routeParams, cityService, weatherService) {
     $scope.city = cityService.city;
     $scope.days = $routeParams.days || '1';
 
-    // Fetch API data
-    $scope.weatherAPI = $resource(
-      'http://api.openweathermap.org/data/2.5/forecast/daily',
-      {
-        callback: 'JSON_CALLBACK'
-      },
-      {
-        get: {
-          method: 'JSONP'
-        }
-      }
-    );
-
-    $scope.weatherResult = $scope.weatherAPI.get({
-      q: $scope.city,
-      cnt: $scope.days,
-      APPID: '279b4be6d54c8bf6ea9b12275a567156'
-    });
+    // Use our custom (reusable) service to get weather data
+    $scope.weatherResult = weatherService.GetWeather($scope.city, $scope.days);
 
     $scope.convertToCelsius = function(degK) {
       return Math.round(degK - 273.15);
--- services.js
+++ services.js
@@ -2,3 +2,29 @@
 weatherApp.service('cityService', function() {
   this.city = 'Belgrade';
 });
+
+// Fetch API data SERVICE
+weatherApp.service('weatherService', [
+  '$resource',
+  function($resource) {
+    this.GetWeather = function(city, days) {
+      var weatherAPI = $resource(
+        'http://api.openweathermap.org/data/2.5/forecast/daily',
+        {
+          callback: 'JSON_CALLBACK'
+        },
+        {
+          get: {
+            method: 'JSONP'
+          }
+        }
+      );
+
+      return weatherAPI.get({
+        q: city,
+        cnt: days,
+        APPID: '279b4be6d54c8bf6ea9b12275a567156'
+      });
+    };
+  }
+]);
