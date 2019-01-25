387113ca373800364e734ecb840144de02d44f4c
Jelena Marjanović <jelena.marjanovic.13@gmail.com>
2019-01-25 20:16:14 +0100

    Fetch and bind data using ngResource

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // MODULE
-var weatherApp = angular.module('weatherApp', ['ngRoute']);
+var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
 
 // ROUTES
 weatherApp.config(function($routeProvider) {
@@ -35,8 +35,31 @@ weatherApp.controller('homeController', [
 
 weatherApp.controller('forecastController', [
   '$scope',
+  '$resource',
   'cityService',
-  function($scope, cityService) {
+  function($scope, $resource, cityService) {
     $scope.city = cityService.city;
+    $scope.days = '3';
+
+    // Fetch API data
+    $scope.weatherAPI = $resource(
+      'http://api.openweathermap.org/data/2.5/forecast/daily',
+      {
+        callback: 'JSON_CALLBACK'
+      },
+      {
+        get: {
+          method: 'JSONP'
+        }
+      }
+    );
+
+    $scope.weatherResult = $scope.weatherAPI.get({
+      q: $scope.city,
+      cnt: $scope.days,
+      APPID: '279b4be6d54c8bf6ea9b12275a567156'
+    });
+
+    console.log($scope.weatherResult);
   }
 ]);
